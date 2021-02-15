7f411ae832c9c5f332aac00996735ecbbca5eb1f
jleszkiewicz <72871388+jleszkiewicz@users.noreply.github.com>
2021-02-15 02:17:17 +0100

    Add files via upload

---
+++ src/Firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import 'firebase/firestore'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAV4vHeKU7UFxBhS7teDZYBXyj6k4ztMgU",
+    authDomain: "task-app-2dfba.firebaseapp.com",
+    databaseURL: "https://task-app-2dfba-default-rtdb.europe-west1.firebasedatabase.app/",
+    projectId: "task-app-2dfba",
+    storageBucket: "task-app-2dfba.appspot.com",
+    messagingSenderId: "549313692721",
+    appId: "1:549313692721:web:3de0053d61cda828370110",
+
+};
+
+// Initialize Firebase
+
+export default firebase;
